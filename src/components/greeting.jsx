84f4e50c2c6885ab97e3d4a20fba7f6c0c6fb4cc
Vaclav Ruzicka <vaclavruzicka@usf.edu>
2025-04-06 16:53:19 -0400

    Task 1 - Create a New Component

---
+++ src/components/greeting.jsx
@@ -0,0 +1,17 @@
+//Task 1 - Create a New Component
+
+//Importing React
+import React from "react";
+
+//Creating the fuction Greeting.
+function Greeting() {
+    return (
+        <div>
+        <h2>Hello from the Greeting Component</h2>
+        <p>This is your first custom component in React.</p>
+        </div>
+    );
+}
+
+///Exporting the greeting function.
+export default Greeting;
