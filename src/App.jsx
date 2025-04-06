dd3aaa628b78e617ec4a404b5567d587b1188211
Vaclav Ruzicka <vaclavruzicka@usf.edu>
2025-04-06 17:01:18 -0400

    Task 2 - Using the Component.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+//Importing the Greeting component.
+import Greeting from './components/greeting'
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -17,6 +20,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <Greeting />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -31,5 +35,6 @@ function App() {
     </>
   )
 }
+
 
 export default App
